2d6747f84ef3c56952546fe75862db2c928f2343
jonas <jonas@titan.(none)>
2010-06-21 11:52:48 +0200

    added a basic forfeiting script

---
+++ root/scripts/forfeit.lua
@@ -0,0 +1,31 @@
+function init()
+  Controller:Register("GamePlayerChat", insult_player)
+  forfeit_register = {}
+  gcount = 0
+end
+
+function insult_player(event)
+  if event:GetMessage() ~= "!ff" then return end
+  
+  game = event:GetGame()
+  game_id = game:GetHostCounter()
+  player = event:GetPlayer()
+  team = game:GetTeamOfPlayer(event:GetPlayer())
+  
+  if forfeit_register[game_id] == nil then forfeit_register[game_id] = {} end
+  if forfeit_register[game_id][team] == nil then forfeit_register[game_id][team] = {} end
+  team_register = forfeit_register[game_id][team]
+    
+  found = false
+  for i=0,10 do
+    if team_register[i] == player:GetName() then found = true end
+  end
+  if not found then
+    table.insert(team_register, player:GetName())
+    new_count = #team_register
+    
+    game:SendAllChat(player:GetName() .. " just forfeited. " .. tostring(new_count) .. " players have forfeited so far.")
+  else
+    game:SendAllChat("You have already forfeited, " .. player:GetName())
+  end
+end
